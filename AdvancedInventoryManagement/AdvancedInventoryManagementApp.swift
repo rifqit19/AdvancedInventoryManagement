f167ae5d3feec0a5e750c847ea1a594473e42966
rifqit19 <rifqit19@gmail.com>
2024-12-12 01:08:22 +0700

    initial project

---
+++ AdvancedInventoryManagement/AdvancedInventoryManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedInventoryManagementApp.swift
+//  AdvancedInventoryManagement
+//
+//  Created by rifqi triginandri on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedInventoryManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
