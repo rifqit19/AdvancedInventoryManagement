354639260fe9c93530db0ed4db1cfa83ad645227
rifqit19 <rifqit19@gmail.com>
2024-12-22 23:29:56 +0700

    create model for supplier

---
+++ AdvancedInventoryManagement/Models/Supplier.swift
@@ -0,0 +1,15 @@
+//
+//  Supplier.swift
+//  AdvancedInventoryManagement
+//
+//  Created by rifqi triginandri on 15/12/24.
+//
+
+struct Supplier: Identifiable {
+    let id: String
+    let name: String
+    let address: String
+    let contact: String
+    let latitude: Double
+    let longitude: Double
+}
