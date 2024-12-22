2009d4a5aa653f786b26145516fb1385d641e474
rifqit19 <rifqit19@gmail.com>
2024-12-22 23:26:20 +0700

    [UI] Create accout view

---
+++ AdvancedInventoryManagement/Moduls/Account/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  AdvancedInventoryManagement
+//
+//  Created by rifqi triginandri on 22/12/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
