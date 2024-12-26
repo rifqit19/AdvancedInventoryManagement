209ae4f362a5f1bebb70658f5775efa335f7b322
rifqit19 <rifqit19@gmail.com>
2024-12-26 16:47:52 +0700

    modified ContentView

--- AdvancedInventoryManagement/Moduls/ContentView.swift
+++ AdvancedInventoryManagement/Moduls/ContentView.swift
@@ -10,32 +10,32 @@ import SwiftUI
 // User interfase for dasboard
 struct ContentView: View {
     
-    @EnvironmentObject var viewModel: AuthViewModel
+    @EnvironmentObject var authViewModel: AuthViewModel
     @StateObject private var supplierViewModel = SupplierViewModel()
-    
+    @StateObject private var itemViewModel = ItemViewModel()
+
     var body: some View {
         Group {
-            if viewModel.userSession != nil {
+            if authViewModel.userSession != nil {
                 TabView {
-                    ItemListView()
-                        .tabItem {
-                            Label("Inventory", systemImage: "doc.text")
-                        }
-
-        //            InventoryView()
-        //                .tabItem {
-        //                    Label("Inventory", systemImage: "doc.text")
-        //                }
-                    
                     SupplierView()
                         .tabItem {
                             Label("Supplier", systemImage: "list.dash")
-                        }.environmentObject(supplierViewModel)
+                        }
+                        .environmentObject(supplierViewModel)
+                        .environmentObject(itemViewModel)
+
+                    ItemListView()
+                        .tabItem {
+                            Label("Inventory", systemImage: "doc.text")
+                        }.environmentObject(itemViewModel)
+                    
                     
                     AccountView()
                         .tabItem {
                             Label("Account", systemImage: "person")
                         }
+
                 }
             } else {
                 LoginView()
