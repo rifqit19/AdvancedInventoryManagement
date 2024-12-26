33035624259058cbde0cace4909da190d1708e68
rifqit19 <rifqit19@gmail.com>
2024-12-26 16:45:04 +0700

    modified supplier model

--- AdvancedInventoryManagement/Models/Supplier.swift
+++ AdvancedInventoryManagement/Models/Supplier.swift
@@ -16,7 +16,7 @@ struct Supplier: Identifiable, Codable {
     var contact: String
     var longitude: Double
     var latitude: Double
-    var items: [Item] = [] // Barang di dalam supplier
+    var items: [SQLItem] = []
     
     // Custom initializer
     init(id: String, userID: String, name: String, address: String, contact: String, longitude: Double, latitude: Double) {
@@ -39,30 +39,30 @@ struct Supplier: Identifiable, Codable {
         case latitude
     }
     
-    // Manual Decoding
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decodeIfPresent(String.self, forKey: .id)
-        self.userID = try container.decode(String.self, forKey: .userID)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.address = try container.decode(String.self, forKey: .address)
-        self.contact = try container.decode(String.self, forKey: .contact)
-        self.longitude = try container.decode(Double.self, forKey: .longitude)
-        self.latitude = try container.decode(Double.self, forKey: .latitude)
-        self.items = [] // Fetch manually if necessary
-    }
-    
-    // Manual Encoding
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encodeIfPresent(id, forKey: .id)
-        try container.encode(userID, forKey: .userID)
-        try container.encode(name, forKey: .name)
-        try container.encode(address, forKey: .address)
-        try container.encode(contact, forKey: .contact)
-        try container.encode(longitude, forKey: .longitude)
-        try container.encode(latitude, forKey: .latitude)
-    }
+//    // Manual Decoding
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.id = try container.decodeIfPresent(String.self, forKey: .id)
+//        self.userID = try container.decode(String.self, forKey: .userID)
+//        self.name = try container.decode(String.self, forKey: .name)
+//        self.address = try container.decode(String.self, forKey: .address)
+//        self.contact = try container.decode(String.self, forKey: .contact)
+//        self.longitude = try container.decode(Double.self, forKey: .longitude)
+//        self.latitude = try container.decode(Double.self, forKey: .latitude)
+//        self.items = [] // Fetch manually if necessary
+//    }
+//    
+//    // Manual Encoding
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encodeIfPresent(id, forKey: .id)
+//        try container.encode(userID, forKey: .userID)
+//        try container.encode(name, forKey: .name)
+//        try container.encode(address, forKey: .address)
+//        try container.encode(contact, forKey: .contact)
+//        try container.encode(longitude, forKey: .longitude)
+//        try container.encode(latitude, forKey: .latitude)
+//    }
 }
 
 struct Supplier2: Identifiable {
