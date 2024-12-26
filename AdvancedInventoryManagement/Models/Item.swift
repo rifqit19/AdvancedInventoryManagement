3f1a974bec1b3dd8ce8bc1b8bd33eddbf5b3046f
rifqit19 <rifqit19@gmail.com>
2024-12-26 16:44:26 +0700

    create model for inventory item

--- AdvancedInventoryManagement/Models/Item.swift
+++ AdvancedInventoryManagement/Models/Item.swift
@@ -2,16 +2,35 @@
 //  Item.swift
 //  AdvancedInventoryManagement
 //
-//  Created by rifqi triginandri on 18/12/24.
+//  Created by rifqi triginandri on 25/12/24.
 //
 
-struct Item: Identifiable {
-    var id: Int
+
+import Foundation
+import FirebaseFirestore
+
+struct Item: Identifiable, Codable {
+    @DocumentID var id: String?
     var name: String
     var description: String
     var category: String
     var price: Double
     var stock: Int
-    var imagePath: String
-}
+    var imageURL: String?
+    var supplierID: String
+    var supplierName: String
+    var userID: String
 
+    init(id: String? = nil, name: String, description: String, category: String, price: Double, stock: Int, imageURL: String? = nil, supplierID: String, supplierName: String, userID: String) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.price = price
+        self.stock = stock
+        self.imageURL = imageURL
+        self.supplierID = supplierID
+        self.supplierName = supplierName
+        self.userID = userID
+    }
+}
