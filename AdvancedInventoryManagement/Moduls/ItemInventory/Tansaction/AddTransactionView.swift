12648b3315b777899aaf0404355003ea3ef6f7fa
rifqit19 <rifqit19@gmail.com>
2024-12-27 17:36:40 +0700

    add alert for transactions when items are missing for outgoing transactions

--- AdvancedInventoryManagement/Moduls/ItemInventory/Tansaction/AddTransactionView.swift
+++ AdvancedInventoryManagement/Moduls/ItemInventory/Tansaction/AddTransactionView.swift
@@ -16,10 +16,14 @@ struct AddTransactionView: View {
     var supplierID: String
     var itemID: String
     var itemName: String
+    var currentStock: Int
 
     @State private var transactionType = "Masuk"
     @State private var quantity = ""
     @State private var date = Date()
+    
+    @State private var showAlert = false
+    @State private var alertMessage = ""
 
     var body: some View {
         VStack {
@@ -45,6 +49,12 @@ struct AddTransactionView: View {
             Button(action: {
                 let quantityValue = Int(quantity) ?? 0
                 
+                if transactionType == "Keluar" && quantityValue > currentStock {
+                    alertMessage = "Out of stock."
+                    showAlert = true
+                    return
+                }
+
                 let transaction = Transaction(itemId: itemID, itemName: itemName, type: transactionType, quantity: transactionType == "Masuk" ? quantityValue : -quantityValue, date: date, supplierID: supplierID)
                 print("cek transaksi: \(transaction)")
 
@@ -63,11 +73,16 @@ struct AddTransactionView: View {
                     .cornerRadius(20)
             }
             .padding()
+            
         }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Warning!"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+        }
+
     }
 }
 
 #Preview {
 //    let sampleViewModel = InventoryViewModel()
-    AddTransactionView(isPresented: .constant(true), supplierID: "", itemID: "", itemName: "Beras")
+    AddTransactionView(isPresented: .constant(true), supplierID: "", itemID: "", itemName: "Beras", currentStock: 10)
 }
