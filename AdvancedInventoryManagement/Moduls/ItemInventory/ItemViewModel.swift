dc34d76772d812126e823561b4a42d1c37d7b4aa
rifqit19 <rifqit19@gmail.com>
2024-12-27 17:34:57 +0700

    create function for editItem, delete transaction collection when item deleted, and delete image item from storage

--- AdvancedInventoryManagement/Moduls/ItemInventory/ItemViewModel.swift
+++ AdvancedInventoryManagement/Moduls/ItemInventory/ItemViewModel.swift
@@ -10,10 +10,12 @@ import Foundation
 import FirebaseCore
 import FirebaseStorage
 import FirebaseFirestore
+import FirebaseAuth
 
 @MainActor
 class ItemViewModel: ObservableObject {
     @Published var items: [Item] = []
+//    @Published var totalItems: Int = 0
     
     private let db = Firestore.firestore()
     private let storage = Storage.storage()
@@ -25,7 +27,7 @@ class ItemViewModel: ObservableObject {
 //        }
 //    }
 
-    // Mendapatkan semua item
+    // fetch all item
     func fetchItems() {
         items.removeAll()
         db.collectionGroup("items").addSnapshotListener { (querySnapshot, error) in
@@ -33,14 +35,13 @@ class ItemViewModel: ObservableObject {
                 print("No documents")
                 return
             }
+            
             self.items = documents.compactMap { try? $0.data(as: Item.self) }
 
-            // Print all fetched items
             print("DEBUG: Fetched all items:")
             for item in self.items {
                 print("- \(item.name): Quantity \(item.stock), SupplierID \(item.supplierID),  userID \(item.userID)")
             }
-
         }
     }
 
@@ -56,7 +57,7 @@ class ItemViewModel: ObservableObject {
                     return
                 }
                 self.items = documents.compactMap { try? $0.data(as: Item.self) }
-                // Print all fetched items
+
                 print("DEBUG: Fetched all items:")
                 for item in self.items {
                     print("- \(item.name): Quantity \(item.stock), SupplierID \(item.supplierID),  userID \(item.userID)")
@@ -64,44 +65,75 @@ class ItemViewModel: ObservableObject {
 
             }
     }
+    
 
     // Add a new item to a supplier
     func addItem(to supplierID: String, item: Item, imageData: Data) async {
         do {
             // Upload image to Firebase Storage
             let imageRef = storage.reference().child("itemImages/\(UUID().uuidString).jpg")
-            let _ = try await imageRef.putDataAsync(imageData)
-            let imageURL = try await imageRef.downloadURL().absoluteString
+            let _ = try await imageRef.putDataAsync(imageData) // upload umage
+            let imageURL = try await imageRef.downloadURL().absoluteString // get URL
             
             // Add item to Firestore
             var newItem = item
             newItem.imageURL = imageURL
             let _ = try db.collection("suppliers").document(supplierID).collection("items").addDocument(from: newItem)
             
-//            await fetchItems(for: supplierID)
         } catch {
             print("DEBUG: Failed to add item \(error.localizedDescription)")
         }
     }
 
-    
-    // MARK: unused function
     // Update an item for a specific supplier
-    func updateItem(for supplierID: String, item: Item) async {
+    func editItem(for supplierID: String, item: Item, newImageData: Data?) async {
         guard let id = item.id else { return }
         do {
-            try db.collection("suppliers").document(supplierID).collection("items").document(id).setData(from: item)
+            var updatedItem = item
+            
+            if let newImageData = newImageData {
+                if let oldImageURL = item.imageURL {
+                    let oldImageRef = storage.reference(forURL: oldImageURL)
+                    try await oldImageRef.delete()
+                }
+
+                let imageRef = storage.reference().child("itemImages/\(UUID().uuidString).jpg")
+                let _ = try await imageRef.putDataAsync(newImageData)
+                let newImageURL = try await imageRef.downloadURL().absoluteString
+                updatedItem.imageURL = newImageURL
+            }
+            
+            try db.collection("suppliers").document(supplierID).collection("items").document(id).setData(from: updatedItem)
+            
             await fetchItems(for: supplierID)
         } catch {
-            print("DEBUG: Failed to update item \(error.localizedDescription)")
+            print("DEBUG: Failed to edit item \(error.localizedDescription)")
         }
     }
     
     // Delete an item from a supplier
-    func deleteItem(from supplierID: String, itemID: String) async {
+    func deleteItem(from supplierID: String, imageURL : String?, itemID: String) async {
         do {
+            
+            let transactionsCollection = db.collection("suppliers")
+                .document(supplierID)
+                .collection("items")
+                .document(itemID)
+                .collection("transactions")
+            
+            let snapshot = try await transactionsCollection.getDocuments()
+            for document in snapshot.documents {
+                try await document.reference.delete()
+            }
+
+            if let oldImageURL = imageURL {
+                let oldImageRef = storage.reference(forURL: oldImageURL)
+                try await oldImageRef.delete()
+            }
+
+
             try await db.collection("suppliers").document(supplierID).collection("items").document(itemID).delete()
-            await fetchItems(for: supplierID)
+            
         } catch {
             print("DEBUG: Failed to delete item \(error.localizedDescription)")
         }
