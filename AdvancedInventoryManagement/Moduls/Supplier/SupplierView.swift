696920d65207cb65cfbf290d0670ff6e512d0e30
rifqit19 <rifqit19@gmail.com>
2024-12-27 17:30:34 +0700

    remove dummy data suppliers and items

--- AdvancedInventoryManagement/Moduls/Supplier/SupplierView.swift
+++ AdvancedInventoryManagement/Moduls/Supplier/SupplierView.swift
@@ -8,20 +8,6 @@
 import SwiftUI
 
 struct SupplierView: View {
-    // Dummy data for suppliers
-    @State private var suppliers: [Supplier2] = [
-        Supplier2(id: "1", name: "Tech Supplies Inc.", address: "123 Tech Street", contact: "+123456789", latitude: 37.7749, longitude: -122.4194),
-        Supplier2(id: "2", name: "Furniture Co.", address: "456 Home Ave", contact: "+987654321", latitude: 34.0522, longitude: -118.2437),
-        Supplier2(id: "3", name: "Office Needs", address: "789 Office Blvd", contact: "+1122334455", latitude: 40.7128, longitude: -74.0060)
-    ]
-
-    // Dummy data for inventory items
-    @State private var inventoryItems: [Inventory] = [
-        Inventory(id: "1", name: "Laptop", quantity: 10, supplierID: "1", supplierName: "Tech Supplies Inc."),
-        Inventory(id: "2", name: "Office Chair", quantity: 5, supplierID: "2", supplierName: "Furniture Co."),
-        Inventory(id: "3", name: "Projector", quantity: 2, supplierID: "1", supplierName: "Tech Supplies Inc."),
-        Inventory(id: "4", name: "Desk Lamp", quantity: 8, supplierID: "3", supplierName: "Office Needs")
-    ]
     
     @EnvironmentObject var viewModel: SupplierViewModel
     
@@ -29,7 +15,7 @@ struct SupplierView: View {
         NavigationView {
             ZStack{
                 
-                if viewModel.suppliers.isEmpty{
+                if viewModel.suppliers.isEmpty {
                     VStack {
                         Spacer()
                         
@@ -55,7 +41,7 @@ struct SupplierView: View {
 
                 } else {
                     List(viewModel.suppliers) { supplier in
-                        NavigationLink(destination: SupplierDetailView(supplier: supplier)) {
+                        NavigationLink(destination: SupplierDetailView(supplier: .constant(supplier))) {
                             VStack(alignment: .leading) {
                                 Text(supplier.name)
                                     .font(.headline)
