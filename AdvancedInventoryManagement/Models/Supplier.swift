f65078ee6c13f0fcb3715eba8ebd5159490bf89d
rifqit19 <rifqit19@gmail.com>
2024-12-27 17:26:21 +0700

    delete sqlItem object from supplier model

--- AdvancedInventoryManagement/Models/Supplier.swift
+++ AdvancedInventoryManagement/Models/Supplier.swift
@@ -8,7 +8,7 @@
 import Foundation
 import FirebaseFirestore
 
-struct Supplier: Identifiable, Codable {
+struct Supplier: Identifiable, Codable, Equatable {
     @DocumentID var id: String? // Firestore document ID
     var userID: String
     var name: String
@@ -16,9 +16,7 @@ struct Supplier: Identifiable, Codable {
     var contact: String
     var longitude: Double
     var latitude: Double
-    var items: [SQLItem] = []
     
-    // Custom initializer
     init(id: String, userID: String, name: String, address: String, contact: String, longitude: Double, latitude: Double) {
         self.id = id
         self.userID = userID
@@ -28,48 +26,6 @@ struct Supplier: Identifiable, Codable {
         self.longitude = longitude
         self.latitude = latitude
     }
-
-    enum CodingKeys: String, CodingKey {
-        case id
-        case userID
-        case name
-        case address
-        case contact
-        case longitude
-        case latitude
-    }
     
-//    // Manual Decoding
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        self.id = try container.decodeIfPresent(String.self, forKey: .id)
-//        self.userID = try container.decode(String.self, forKey: .userID)
-//        self.name = try container.decode(String.self, forKey: .name)
-//        self.address = try container.decode(String.self, forKey: .address)
-//        self.contact = try container.decode(String.self, forKey: .contact)
-//        self.longitude = try container.decode(Double.self, forKey: .longitude)
-//        self.latitude = try container.decode(Double.self, forKey: .latitude)
-//        self.items = [] // Fetch manually if necessary
-//    }
-//    
-//    // Manual Encoding
-//    func encode(to encoder: Encoder) throws {
-//        var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encodeIfPresent(id, forKey: .id)
-//        try container.encode(userID, forKey: .userID)
-//        try container.encode(name, forKey: .name)
-//        try container.encode(address, forKey: .address)
-//        try container.encode(contact, forKey: .contact)
-//        try container.encode(longitude, forKey: .longitude)
-//        try container.encode(latitude, forKey: .latitude)
-//    }
 }
 
-struct Supplier2: Identifiable {
-    let id: String
-    let name: String
-    let address: String
-    let contact: String
-    let latitude: Double
-    let longitude: Double
-}
