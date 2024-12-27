e3d998a01325cb63e3c930c01b063811637eb71e
rifqit19 <rifqit19@gmail.com>
2024-12-27 17:38:49 +0700

    create button edit & delete, implement delete supplier, create binding for mapView for update map region after update supplier location

--- AdvancedInventoryManagement/Moduls/Supplier/SupplierDetailView.swift
+++ AdvancedInventoryManagement/Moduls/Supplier/SupplierDetailView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 import MapKit
 
 struct SupplierDetailView: View {
-    let supplier: Supplier
+    @Binding var supplier: Supplier
     
     @ObservedObject var itemViewModel = ItemViewModel()
-
     @State private var region: MKCoordinateRegion
+    @EnvironmentObject var supplierViewModel: SupplierViewModel
 
-    init(supplier: Supplier) {
-        self.supplier = supplier
+    @Environment(\.dismiss) var dismiss
+
+    init(supplier: Binding<Supplier>) {
+        self._supplier = supplier
         _region = State(initialValue: MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: supplier.latitude, longitude: supplier.longitude),
+            center: CLLocationCoordinate2D(latitude: supplier.wrappedValue.latitude, longitude: supplier.wrappedValue.longitude),
             span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         ))
     }
@@ -26,7 +28,7 @@ struct SupplierDetailView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
-                HStack{
+                HStack {
                     Spacer()
                     Text("Details of Supplier")
                         .font(.title2)
@@ -44,9 +46,7 @@ struct SupplierDetailView: View {
                 Text("Contact: \(supplier.contact)")
                     .font(.body)
 
-                ZStack{
-                    // buatkan mapview
-                    
+                ZStack {
                     Map(coordinateRegion: $region, annotationItems: [supplier]) { location in
                         MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
                     }
@@ -55,21 +55,47 @@ struct SupplierDetailView: View {
                     .shadow(radius: 5)
                     .disabled(true)
                     
-
-                    
                     Button(action: {
                         openGoogleMaps(lat: supplier.latitude, lng: supplier.longitude)
                     }) {
                         Rectangle()
                             .foregroundColor(.clear)
                             .contentShape(Rectangle())
-
                     }
                 }
                 
                 Text("Lat: \(supplier.latitude), Long: \(supplier.longitude)")
                     .font(.body)
 
+                HStack {
+                    Button(action: {
+                        Task {
+                            await supplierViewModel.deleteSupplier(supplierID: supplier.id ?? "")
+                            dismiss()
+                        }
+                    }) {
+                        Text("Delete Supplier")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(.red)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                    }
+
+                    NavigationLink(destination: EditSupplierView(supplier: supplier).onDisappear(perform: {
+                        itemViewModel.fetchItems(for: supplier.id ?? "")
+                    })) {
+                        Text("Edit Supplier")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                    }
+                }
+                .padding(.vertical)
 
                 Divider()
 
@@ -80,61 +106,57 @@ struct SupplierDetailView: View {
                         NavigationLink(destination: AddItemView(supplierID: supplier.id ?? "", supplierName: supplier.name)) {
                             Image(systemName: "plus")
                         }
-                        
                     }
                     
-                    // tampilkan list items
                     if itemViewModel.items.isEmpty {
                         Text("No items available.")
                             .foregroundColor(.gray)
                             .italic()
                     } else {
                         ForEach(itemViewModel.items) { item in
-                            NavigationLink(destination: ItemDetailView(item: item)){
-                                       HStack(alignment: .top, spacing: 16) {
-                                           if let imageURL = item.imageURL, let url = URL(string: imageURL) {
-                                               AsyncImage(url: url) { image in
-                                                   image
-                                                       .resizable()
-                                                       .aspectRatio(contentMode: .fill)
-                                                       .frame(width: 60, height: 60)
-                                                       .cornerRadius(8)
-                                                       .shadow(radius: 2)
-                                               } placeholder: {
-                                                   ProgressView()
-                                                       .frame(width: 60, height: 60)
-                                               }
-                                           } else {
-                                               Rectangle()
-                                                   .fill(Color.gray.opacity(0.3))
-                                                   .frame(width: 60, height: 60)
-                                                   .cornerRadius(8)
-                                                   .overlay(Text("No Image").font(.caption).foregroundColor(.gray))
-                                           }
-                                           
-                                           // Informasi Item
-                                           VStack(alignment: .leading, spacing: 8) {
-                                               Text(item.name)
-                                                   .font(.headline)
-                                                   .lineLimit(1)
-                                               
-                                               Text("Price: Rp.\(item.price, specifier: "%.2f")")
-                                                   .font(.subheadline)
-                                                   .foregroundColor(.secondary)
-                                               
-                                               Text("Stock: \(item.stock)")
-                                                   .font(.subheadline)
-                                                   .foregroundColor(item.stock > 0 ? .green : .red)
-                                               
-                                               Text("Category: \(item.category)")
-                                                   .font(.subheadline)
-                                                   .foregroundColor(.secondary)
-                                               
-                                           }
-                                       }
-                                       .padding(.vertical, 8)
-                                   }
-                               }
+                            NavigationLink(destination: ItemDetailView(item: item)) {
+                                HStack(alignment: .top, spacing: 16) {
+                                    if let imageURL = item.imageURL, let url = URL(string: imageURL) {
+                                        AsyncImage(url: url) { image in
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: 60, height: 60)
+                                                .cornerRadius(8)
+                                                .shadow(radius: 2)
+                                        } placeholder: {
+                                            ProgressView()
+                                                .frame(width: 60, height: 60)
+                                        }
+                                    } else {
+                                        Rectangle()
+                                            .fill(Color.gray.opacity(0.3))
+                                            .frame(width: 60, height: 60)
+                                            .cornerRadius(8)
+                                            .overlay(Text("No Image").font(.caption).foregroundColor(.gray))
+                                    }
+                                    
+                                    VStack(alignment: .leading, spacing: 8) {
+                                        Text(item.name)
+                                            .font(.headline)
+                                            .lineLimit(1)
+                                        
+                                        Text("Price: Rp.\(item.price, specifier: "%.2f")")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                        
+                                        Text("Stock: \(item.stock)")
+                                            .font(.subheadline)
+                                            .foregroundColor(item.stock > 0 ? .green : .red)
+                                        
+                                        Text("Category: \(item.category)")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                    }
+                                }
+                                .padding(.vertical, 8)
+                            }
+                        }
                     }
                 }
 
@@ -144,10 +166,15 @@ struct SupplierDetailView: View {
             .onAppear {
                 itemViewModel.fetchItems(for: supplier.id ?? "")
             }
+            .onChange(of: supplier) { newSupplier in
+                region = MKCoordinateRegion(
+                    center: CLLocationCoordinate2D(latitude: newSupplier.latitude, longitude: newSupplier.longitude),
+                    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                )
+                
+            }
             .hideTabBar()
-            
         }
-
     }
 
     func openGoogleMaps(lat: Double, lng: Double) {
@@ -157,6 +184,7 @@ struct SupplierDetailView: View {
         }
     }
 }
+
 #Preview {
     var sample =  Supplier(
         id: "1",
@@ -167,10 +195,5 @@ struct SupplierDetailView: View {
         longitude: -122.4194,
         latitude: 37.7749)
 
-    var inventoryItems: [Inventory] = [
-        Inventory(id: "1", name: "Laptop", quantity: 10, supplierID: "1", supplierName: "Tech Supplies Inc."),
-        Inventory(id: "3", name: "Projector", quantity: 2, supplierID: "1", supplierName: "Tech Supplies Inc.")
-    ]
-
-    SupplierDetailView(supplier: sample)
+    SupplierDetailView(supplier: .constant(sample))
 }
